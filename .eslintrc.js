811ed4817f1ea8c474d1af3e5cd10fa28f49732b
soulsands <407221377@qq.com>
2020-06-07 21:35:47 +0800

    chore: pritter 升级导致默认配置改变，而lint规则没有改变，导致报出警告

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,26 @@ module.exports = {
   env: {
     node: true,
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   plugins: ["@typescript-eslint"],
   rules: {
     "prettier/prettier": 1,
     eqeqeq: ["warn", "always"],
-    "@typescript-eslint/no-var-requires": 0
+    "@typescript-eslint/no-var-requires": 0,
   },
   parserOptions: {
     ecmaVersion: 6,
     sourceType: "module",
     tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"]
+    project: ["./tsconfig.json"],
   },
-  parser: "@typescript-eslint/parser"
+  parser: "@typescript-eslint/parser",
 };
